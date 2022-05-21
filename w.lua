1274d53d9285d99d560baceb26333e55d71fd737
bubatz <71530056+cynhook@users.noreply.github.com>
2022-05-21 19:05:16 -0400

    Update w.lua

--- w.lua
+++ w.lua
@@ -5,12 +5,4 @@ game.StarterGui:SetCore(
 	Title = "Neverlockin BETA 0.1v",
 	Text = "Edgebugged",
 }
---// Doesn't work if RespectFilteringEnabled is enabled
-print(game:GetService("SoundService").RespectFilteringEnabled)
-
---// Get's every sound instance in workspace and play's it (Earrape btw)
-for _, sound in next, workspace:GetDescendants() do
-   if sound:IsA("Sound") then
-       sound:Play()
-   end
-end
+)
